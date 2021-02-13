4e9622f15df2a0b4948b3629a51771e1a85b9a61
Sebi <stoicescusebastian@yahoo.com>
2021-02-13 20:30:02 +0200

    adding port

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', (req,res) => {
     //res.send(database.users);
 })*/
 
-app.listen(3000, ()=> {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
