4eee464bbe82b5ef8447b46f4ace8a56298e7f4f
Sebi <stoicescusebastian@yahoo.com>
2021-02-13 19:30:11 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'gabriel21',
+      database : 'smart-brain'
+    }
+  });
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+/* SIGN IN */
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt);  });
+
+/* REGISTER */
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) } );
+
+/* IMAGE */
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db)});
+
+/*  
+
+PROFILES
+
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+ 
+    db.select('*').from('users').where({
+        id: id
+    })
+    .then(user=> {
+        if(user.length > 1)
+            res.json(user[0]);
+        else res.status(400).json('Not found')
+    })
+    .catch(err => res.status(400).json('Error getting user'));
+
+})
+*/
+
+/*
+
+ROOT
+
+app.get('/', (req,res) => {
+    //res.send(database.users);
+})*/
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
+
